d613a49bc4071eb5d48e45877669b20bdeb6d2d0
Karol <karolinaq20@hotmail.com>
2021-06-15 16:54:38 -0300

    Feat:Realizada alterações de melhoria no código e feita a documentação no service.

--- ENatu/src/main/java/com/ENatu/ENatu/services/ProdutoServices.java
+++ ENatu/src/main/java/com/ENatu/ENatu/services/ProdutoServices.java
@@ -1,7 +1,5 @@
 package com.ENatu.ENatu.services;
 
-
-
 import java.util.List;
 import java.util.Optional;
 
@@ -12,12 +10,19 @@ import org.springframework.stereotype.Service;
 import com.ENatu.ENatu.model.Produtos;
 import com.ENatu.ENatu.repository.ProdutosRepository;
 
-
 @Service
 public class ProdutoServices {
-	
+
 	private @Autowired ProdutosRepository repository;
-	
+
+	/**
+	 * Metodo utilizado para buscar todos os produtos
+	 * 
+	 * @return Uma lista de produtos
+	 * @since 1.0
+	 * @author EN@tu
+	 */
+
 	public ResponseEntity<List<Produtos>> pegarTodos() {
 		List<Produtos> listaDeProdutos = repository.findAll();
 		if (listaDeProdutos.isEmpty()) {
@@ -27,12 +32,30 @@ public class ProdutoServices {
 			return ResponseEntity.status(200).body(listaDeProdutos);
 		}
 	}
-	
+
+	/**
+	 * Metodo utilizado para buscar produtos por ID
+	 * 
+	 * @param IdProduto
+	 * @return produtos específicos buscando pelo seu ID
+	 * @since 1.0
+	 * @author EN@tu
+	 */
+
 	public ResponseEntity<Produtos> pegarPorId(long idProduto) {
 		return repository.findById(idProduto).map(resp -> ResponseEntity.ok(resp))
 				.orElse(ResponseEntity.notFound().build());
 	}
-	
+
+	/**
+	 * Metodo utilizado para buscar produtos por nome
+	 * 
+	 * @param nomeProduto
+	 * @return produtos específicos buscando pelo seu nome
+	 * @since 1.0
+	 * @author EN@tu
+	 */
+
 	public ResponseEntity<List<Produtos>> pegarNomeProduto(String nomeProduto) {
 
 		List<Produtos> listaDeProdutos = repository.findAllByNomeProdutoContainingIgnoreCase(nomeProduto);
@@ -45,21 +68,38 @@ public class ProdutoServices {
 			return ResponseEntity.status(200).body(listaDeProdutos);
 		}
 	}
-	
-	
-	public ResponseEntity <Object> salvarProduto(Produtos novoProduto){
+
+	/**
+	 * Metodo utilizado para cadastrar produtos
+	 * 
+	 * @param novoProduto
+	 * @return produto é salvo,caso ainda não seja cadastrado, se já estiver
+	 *         cadastrado, não aceita
+	 * @since 1.0
+	 * @author EN@tu
+	 */
+
+	public ResponseEntity<Object> salvarProduto(Produtos novoProduto) {
 		Optional<Produtos> produtoExistente = repository.findByNomeProduto(novoProduto.getNomeProduto());
-		
-		if(produtoExistente.isPresent()) {
+
+		if (produtoExistente.isPresent()) {
 			return ResponseEntity.status(406).body("já cadastrado");
-		}else {
-			return ResponseEntity.status(201).body(repository.save(novoProduto)); 
-		}		
+		} else {
+			return ResponseEntity.status(201).body(repository.save(novoProduto));
+		}
 	}
-	
+
+	/**
+	 * Metodo utilizado para alterar um produto
+	 * 
+	 * @param alterProduto
+	 * @return Uma entidade produto
+	 * @since 1.0
+	 * @author EN@tu
+	 */
+
 	public ResponseEntity<Produtos> atualizarProduto(Long idProduto, Produtos alterProduto) {
 		Optional<Produtos> idProdutoExiste = repository.findById(idProduto);
-		
 
 		if (idProdutoExiste.isPresent()) {
 
@@ -74,16 +114,24 @@ public class ProdutoServices {
 			return ResponseEntity.status(405).build();
 		}
 	}
-	
-	public ResponseEntity<Object> deletarProduto(Long idProduto) {
-        Optional<Produtos> idProdutoExiste = repository.findById(idProduto);
 
-        if (idProdutoExiste.isEmpty()) {
-            return ResponseEntity.status(400).build();
-        } else {
-            repository.deleteById(idProduto);
-            return ResponseEntity.status(200).build();
-        }
-    }
-	
+	/**
+	 * Metodo utilizado para deletar produtos
+	 * 
+	 * @param idProduto
+	 * @return produto é deletado
+	 * @since 1.0
+	 * @author EN@tu
+	 */
+	public ResponseEntity<Object> deletarProduto(Long idProduto) {
+		Optional<Produtos> idProdutoExiste = repository.findById(idProduto);
+
+		if (idProdutoExiste.isEmpty()) {
+			return ResponseEntity.status(400).build();
+		} else {
+			repository.deleteById(idProduto);
+			return ResponseEntity.status(200).build();
+		}
+	}
+
 }
--- ENatu/src/main/java/com/ENatu/ENatu/services/UsuarioServices.java
+++ ENatu/src/main/java/com/ENatu/ENatu/services/UsuarioServices.java
@@ -6,7 +6,6 @@ import java.util.Optional;
 
 import org.apache.commons.codec.binary.Base64;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -20,6 +19,14 @@ public class UsuarioServices {
 
 	private @Autowired UsuarioRepository repository;
 
+	/**
+	 * Metodo utilizado para buscar todos os usuários
+	 * 
+	 * @return Uma lista de usuários
+	 * @since 1.0
+	 * @author EN@tu
+	 */
+
 	public ResponseEntity<List<Usuario>> pegarTodos() {
 		List<Usuario> listaDeNomes = repository.findAll();
 		if (listaDeNomes.isEmpty()) {
@@ -30,10 +37,27 @@ public class UsuarioServices {
 		}
 	}
 
+	/**
+	 * Metodo utilizado para buscar usuários por ID
+	 * 
+	 * @param IdUsuario
+	 * @return usuários específicos buscando pelo seu ID
+	 * @since 1.0
+	 * @author EN@tu
+	 */
 	public ResponseEntity<Usuario> pegarPorId(long idUsuario) {
 		return repository.findById(idUsuario).map(resp -> ResponseEntity.ok(resp))
 				.orElse(ResponseEntity.notFound().build());
 	}
+
+	/**
+	 * Metodo utilizado para buscar usuário por nome
+	 * 
+	 * @param nome
+	 * @return usuários específicos buscando pelo seu nome
+	 * @since 1.0
+	 * @author EN@tu
+	 */
 
 	public ResponseEntity<List<Usuario>> pegarNomeUsuario(String nome) {
 
@@ -47,6 +71,16 @@ public class UsuarioServices {
 			return ResponseEntity.status(200).body(listaDeNomes);
 		}
 	}
+
+	/**
+	 * Metodo utilizado para cadastrar usuários
+	 * 
+	 * @param novoUsuario
+	 * @return usuário é salvo,caso ainda não seja cadastrado, se já estiver
+	 *         cadastrado, não aceita
+	 * @since 1.0
+	 * @author EN@tu
+	 */
 
 	public ResponseEntity<Usuario> cadastrarUsuario(Usuario novoUsuario) {
 		Optional<Usuario> usuarioExistente = repository.findByEmail(novoUsuario.getEmail());
@@ -65,40 +99,32 @@ public class UsuarioServices {
 		}
 	}
 
-	public ResponseEntity<Usuario> atualizarUsuario(Long idUsuario, Usuario alterUsuario) {
-		Optional<Usuario> idUsuarioExiste = repository.findById(idUsuario);
-		Optional<Usuario> emailUsuarioExiste = repository.findByEmail(alterUsuario.getEmail());
-
-		if (idUsuarioExiste.isPresent() && emailUsuarioExiste.isEmpty()) {
-
-			idUsuarioExiste.get().setNome(alterUsuario.getNome());
-			idUsuarioExiste.get().setSenha(alterUsuario.getSenha());
-			/* idUsuarioExiste.get().setEmail(alterUsuario.getEmail()); */
-			return ResponseEntity.status(202).body(repository.save(idUsuarioExiste.get()));
-
-		} else {
-			return ResponseEntity.status(405).build();
-		}
-	}
-	
 	/**
 	 * Metodo utilizado para alterar um usuario
+	 * 
 	 * @param usuarioParaAlterar
 	 * @return Uma entidade Usuario
 	 * @since 1.0
-	 * @author Gabriel
+	 * @author EN@tu
 	 */
-	public ResponseEntity<Usuario> atualizarUsuario(Usuario usuarioParaAlterar){
-		return repository.findByEmail(usuarioParaAlterar.getEmail())
-				.map(usuarioExistente -> {
-					usuarioExistente.setNome(usuarioParaAlterar.getNome());
-					BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-					String senhaCriptografada = encoder.encode(usuarioParaAlterar.getSenha());
-					usuarioExistente.setSenha(senhaCriptografada);
-					return ResponseEntity.status(201).body(repository.save(usuarioExistente));
-				})
-				.orElse(ResponseEntity.status(401).build());
+	public ResponseEntity<Usuario> atualizarUsuario(Usuario usuarioParaAlterar) {
+		return repository.findByEmail(usuarioParaAlterar.getEmail()).map(usuarioExistente -> {
+			usuarioExistente.setNome(usuarioParaAlterar.getNome());
+			BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+			String senhaCriptografada = encoder.encode(usuarioParaAlterar.getSenha());
+			usuarioExistente.setSenha(senhaCriptografada);
+			return ResponseEntity.status(201).body(repository.save(usuarioExistente));
+		}).orElse(ResponseEntity.status(401).build());
 	}
+
+	/**
+	 * Metodo utilizado para deletar usuários
+	 * 
+	 * @param idUsuario
+	 * @return usuário é deletado
+	 * @since 1.0
+	 * @author EN@tu
+	 */
 
 	public ResponseEntity<Object> deletarUsuario(Long idUsuario) {
 		Optional<Usuario> idUsuarioExistente = repository.findById(idUsuario);
@@ -111,56 +137,31 @@ public class UsuarioServices {
 		}
 	}
 
-	public ResponseEntity<Optional<UserLogin>>logar(Optional<UserLogin> user) {
-
-		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-		Optional<Usuario> usuario = repository.findByEmail(user.get().getEmail());
-
-		if (usuario.isPresent()) {
-			if (encoder.matches(user.get().getSenha(), usuario.get().getSenha())) {
-				String auth = user.get().getEmail() + ":" + user.get().getSenha();
-				byte[] encodedAuth = Base64.encodeBase64(auth.getBytes(Charset.forName("US-ASCII")));
-				String authHeader = "Basic " + new String(encodedAuth);
-
-				user.get().setToken(authHeader);
-				user.get().setNome(usuario.get().getNome());
-
-				return ResponseEntity.status(HttpStatus.OK).body(user);
-			} else {
-				return ResponseEntity.status(HttpStatus.NOT_ACCEPTABLE).build();
-			}
-		} else {
-			return ResponseEntity.status(HttpStatus.NOT_ACCEPTABLE).build();
-
-		}
-	}
-	
 	/**
 	 * Metodo utilizado para fazer Autenticação do usuario
+	 * 
 	 * @param usuarioParaAutenticar do tipo UserLogin
 	 * @return UserLogin com atributos preenchidos
 	 * @since 1.0
-	 * @author Gabriel
+	 * @author EN@tu
 	 */
-	public ResponseEntity<?> logarUsuario(UserLogin usuarioParaAutenticar){
-		return repository.findByEmail(usuarioParaAutenticar.getEmail())
-				.map(usuarioExistente -> {
-					BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-					
-					if (encoder.matches(usuarioParaAutenticar.getSenha(), usuarioExistente.getSenha())) {
-						String estruturaBasic = usuarioParaAutenticar.getEmail() + ":" + usuarioParaAutenticar.getSenha();
-						byte[] autorizacaoBasic64 = Base64.encodeBase64(estruturaBasic.getBytes(Charset.forName("US-ASCII")));
-						String autorizacaoHeader = "Basic " + new String(autorizacaoBasic64);
-						
-						usuarioParaAutenticar.setToken(autorizacaoHeader);
-						usuarioParaAutenticar.setNome(usuarioExistente.getNome());
-						usuarioParaAutenticar.setSenha(usuarioExistente.getSenha());
-						return ResponseEntity.status(200).body(usuarioParaAutenticar);					
-					} else {
-						return ResponseEntity.status(401).build();
-					}
-				})
-				.orElse(ResponseEntity.status(401).build());
+	public ResponseEntity<?> logarUsuario(UserLogin usuarioParaAutenticar) {
+		return repository.findByEmail(usuarioParaAutenticar.getEmail()).map(usuarioExistente -> {
+			BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+
+			if (encoder.matches(usuarioParaAutenticar.getSenha(), usuarioExistente.getSenha())) {
+				String estruturaBasic = usuarioParaAutenticar.getEmail() + ":" + usuarioParaAutenticar.getSenha();
+				byte[] autorizacaoBasic64 = Base64.encodeBase64(estruturaBasic.getBytes(Charset.forName("US-ASCII")));
+				String autorizacaoHeader = "Basic " + new String(autorizacaoBasic64);
+
+				usuarioParaAutenticar.setToken(autorizacaoHeader);
+				usuarioParaAutenticar.setNome(usuarioExistente.getNome());
+				usuarioParaAutenticar.setSenha(usuarioExistente.getSenha());
+				return ResponseEntity.status(200).body(usuarioParaAutenticar);
+			} else {
+				return ResponseEntity.status(401).build();
+			}
+		}).orElse(ResponseEntity.status(401).build());
 	}
-	
+
 }
