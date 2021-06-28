87b1789e90f04e96de7c4cd2af3df6db7b9e27df
Bruno Afonso <bruno_afonso_henrique@outlook.com>
2021-06-28 18:01:26 -0300

    feat: Reorganização dos arquivos de backend e incluso os arquivos da primeita task de front

---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { RodapeComponent } from './rodape/rodape.component';
+import { CadastroComponent } from './cadastro/cadastro.component';
+import { EntrarComponent } from './entrar/entrar.component';
+import { HomeComponent } from './home/home.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MenuComponent,
+    RodapeComponent,
+    CadastroComponent,
+    EntrarComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
