101f58adfa34fefe8e8049e208bbd056b32391c9
ismarps <ismarprobst@gmail.com>
2021-07-06 15:05:19 -0300

    Feat CRUD temas e paginas admin

---
+++ frontend/src/app/admin-produto/admin-produto.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { environment } from 'src/environments/environment.prod';
+import { Produtos } from '../model/Produtos';
+import { AuthService } from '../service/auth.service';
+import { ProdutosService } from '../service/produtos.service';
+
+@Component({
+  selector: 'app-admin-produto',
+  templateUrl: './admin-produto.component.html',
+  styleUrls: ['./admin-produto.component.css']
+})
+export class AdminProdutoComponent implements OnInit {
+
+  produto: Produtos = new Produtos()
+  listaProdutos: Produtos[]
+
+  constructor(public auth: AuthService, 
+    private router: Router, 
+    private produtosService: ProdutosService
+    ) { }
+
+  ngOnInit() {
+    if (environment.token == ''){
+      //alert('Sua sessão expirou, faça login novamente.')
+      this.router.navigate(['/entrar'])
+    }
+
+    this.produtosService.refreshToken()
+  }
+
+  cadastrarProduto(){
+    this.produtosService.postProduto(this.produto).subscribe((resp: Produtos)=>{
+      this.produto = resp
+      alert("Produto cadastrado com sucesso")
+      this.produto = new Produtos()
+    })
+  }
+
+}
